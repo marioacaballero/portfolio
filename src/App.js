7e6181aedfb46943dec5f64b4ddfa4103e4d86f3
Mario <mario.caballero.948@gmail.com>
2022-11-28 23:35:22 -0300

    fix deploy

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React a
+          Learn React aa
         </a>
       </header>
     </div>
