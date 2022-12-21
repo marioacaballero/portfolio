b908efa143323efed0cea98a02d7da2fad9667d4
Mario <mario.caballero.948@gmail.com>
2022-12-21 09:02:07 -0300

    set App.js to render Home.jsx component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 /* eslint-disable linebreak-style */
-import About from './components/About/About';
-import Education from './components/Education/Education';
-import Projects from './components/Projects/Projects';
-import Stack from './components/Stack/Stack';
+
+import Home from './components/Home/Home';
 
 function App() {
   console.log('hola');
   return (
     <div>
-      <header>
-        <p>Hello World!</p>
-      </header>
-      <About />
-      <Stack />
-      <Education />
-      <Projects />
+      <Home />
     </div>
   );
 }
