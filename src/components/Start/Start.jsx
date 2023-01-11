45e509b2ed3df1526ce7ce47880541d25b542edd
Mario <mario.caballero.948@gmail.com>
2023-01-11 18:39:52 -0300

    add start component to render first

---
+++ src/components/Start/Start.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import style from './Start.module.css';
+
+function Start() {
+  return (
+    <div className={style.mainStart}>
+      <header>
+        <p>Hello World! Welcome, I am</p>
+      </header>
+      <h1>Mario Alberto Caballero</h1>
+      <h3>Full Stack Web Developer and Engineer</h3>
+    </div>
+  );
+}
+
+export default Start;
