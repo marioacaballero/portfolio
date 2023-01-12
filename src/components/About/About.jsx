96bca58df1b0e8f4e6af6360a59da47bcf7c71d1
Mario <mario.caballero.948@gmail.com>
2023-01-12 12:29:39 -0300

    fix style on about component and change the size of the other img background component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,26 +1,30 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
 import style from './About.module.css';
+import profile from '../Data/galaxy1.jpg';
 
 function About() {
   return (
     <main className={style.mainAbout}>
-      <h2>Mario Alberto Caballero</h2>
-      <h3>Full Stack Web Developer and Engineer</h3>
-      <a href="/">Download CV</a>
+      {/* <h3>Get To Know</h3> */}
+      <h2>-- About Me --</h2>
+      {/* <a href="/">Download CV</a> */}
       <section>
-        I am a Full Stack Developer specialize in web design. I am a good
-        solving problems, creative, proactive and I have a great autonomy, and I
-        enjoy working in teams.
-        <br />
-        I lead the started of an E-commerce related to vegetarian food located
-        on Rosario, Argentina.
-        <br />
-        In my free time I like to practice some sports and read books to keep my
-        body and my mind healthy.
-        <br />
-        You can find in me a good criticism, automation on all things that I do
-        and a great leadership.
+        <img alt="profile" src={profile} />
+        <p>
+          I am a Full Stack Developer specialize in web design. I am a good
+          solving problems, creative, proactive and I have a great autonomy, and
+          I enjoy working in teams.
+          <br />
+          I lead the started of an E-commerce related to vegetarian food located
+          on Rosario, Argentina.
+          <br />
+          In my free time I like to practice some sports and read books to keep
+          my body and my mind healthy.
+          <br />
+          You can find in me a good criticism, automation on all things that I
+          do and a great leadership.
+        </p>
       </section>
     </main>
   );
