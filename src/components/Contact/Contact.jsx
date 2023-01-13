a8af724e91fb93f633f2038dfe9c24e9ace289cc
Mario <mario.caballero.948@gmail.com>
2023-01-13 01:56:15 -0300

    add validator on form in contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,39 +1,94 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import style from './Contact.module.css';
+import { validator } from './validators';
 
 function Contact() {
   const form = useRef();
+  const initialState = {
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  };
+
+  const [input, setInput] = useState(initialState);
+  const [errors, setErrors] = useState({});
+
+  const handleInputChange = (e) => {
+    e.preventDefault();
+    setErrors(
+      validator({
+        ...input,
+        [e.target.name]: e.target.value,
+      })
+    );
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs
-      .sendForm(
-        'service_yqqib8o',
-        'template_cuiti3d',
-        form.current,
-        'Q1V9qOOYJMrSaiOLu'
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-
-    e.target.reset();
+    if (!errors.email && !errors.subject && !errors.message && input.message) {
+      emailjs
+        .sendForm(
+          'service_yqqib8o',
+          'template_cuiti3d',
+          form.current,
+          'Q1V9qOOYJMrSaiOLu'
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+      setInput(initialState);
+      alert('listo');
+    } else {
+      alert('necesitas escribir');
+    }
   };
 
   return (
     <div className={style.mainContact}>
       <h2>-- Contact Me --</h2>
       <form ref={form} onSubmit={sendEmail}>
-        <input type="text" name="name" placeholder="Name" />
-        <input type="email" name="email" placeholder="Email Address" />
-        <input type="text" name="subject" placeholder="Subject" />
-        <textarea name="message" placeholder="Your message" />
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={input.name}
+          onChange={(e) => handleInputChange(e)}
+        />
+        <input
+          type="text"
+          name="subject"
+          value={input.subject}
+          placeholder="Subject"
+          onChange={(e) => handleInputChange(e)}
+        />
+        {errors.subject && <p>{errors.subject}</p>}
+        <input
+          type="email"
+          name="email"
+          value={input.email}
+          placeholder="Email Address"
+          onChange={(e) => handleInputChange(e)}
+        />
+        {errors.email && <p>{errors.email}</p>}
+        <textarea
+          name="message"
+          value={input.message}
+          placeholder="Your message"
+          onChange={(e) => handleInputChange(e)}
+        />
+        {errors.message && <p>{errors.message}</p>}
         <button type="submit">Send Message</button>
       </form>
     </div>
